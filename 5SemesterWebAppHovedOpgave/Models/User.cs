6e2dc266064bbf8cfa43a329e1e0cb39787c7195
Søren Lykke Christensen <soer424c@edu.easj.dk>
2019-11-27 13:49:16 +0100

    Model classes have been added to the Applitcation

---
+++ 5SemesterWebAppHovedOpgave/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace _5SemesterWebAppHovedOpgave.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
--- 5SemesterWebAppHovedOpgave/Startup.cs
+++ 5SemesterWebAppHovedOpgave/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +34,9 @@ namespace _5SemesterWebAppHovedOpgave
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
